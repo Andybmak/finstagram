e07a2e45fa050b97e1dc978de738f13dcfac344a
Andybmak <amakela@enmax.com>
2022-08-04 01:05:24 +0000

    W5D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,19 +1,29 @@
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
+end
+
 helpers do
     def current_user
         User.find_by(id: session[:user_id])
     end
 end
 
+# Controller (list of actions/routes)
+
+# Index page (read)
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
 end
 
+# Signup page (read)
 get '/signup' do
     @user = User.new
     erb(:signup)
 end
 
+# Signup a User (create)
 post '/signup' do
     
     email       = params[:email]
@@ -30,10 +40,12 @@ post '/signup' do
     end
 end
 
+# Login page
 get '/login' do
     erb(:login)
 end
 
+# Login a User
 post '/login' do
     username = params[:username]
     password = params[:password]
@@ -50,11 +62,32 @@ post '/login' do
 
 end
 
+# Logout a User
 get '/logout' do
     session[:user_id] = nil
     redirect to('/')
 end
 
+#FinstagramPost form
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+# Create a FinstagramPost
+post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+  
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+  
+    if @finstagram_post.save
+      redirect(to('/'))
+    else
+      erb(:"finstagram_posts/new")
+end
+
+
+end
 
 
 
